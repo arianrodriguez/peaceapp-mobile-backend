efe0c6983469397e8f0cea9edc4be688ea5117dd
IanMQ <129208479+IanMQ@users.noreply.github.com>
2024-09-10 20:13:31 -0500

    feat(user): add lastname and phonenumber fields to User model

--- src/main/java/com/upc/pre/peaceapp/models/User.java
+++ src/main/java/com/upc/pre/peaceapp/models/User.java
@@ -10,16 +10,22 @@ public class User extends AuditableAbstractAggregateRoot<User> {
     private String name;
     private String email;
     private String password;
+    private String lastname;
+    private String phonenumber;
 
     public User() {
         this.name = "";
         this.email = "";
         this.password = "";
+        this.lastname = "";
+        this.phonenumber = "";
     }
 
-    public User(String name, String email, String password) {
+    public User(String name, String email, String password, String lastname, String phonenumber) {
         this.name = name;
         this.email = email;
         this.password = password;
+        this.lastname = lastname;
+        this.phonenumber = phonenumber;
     }
 }
